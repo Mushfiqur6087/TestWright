ee202a9e8fdcc9c6f60c3c2f66c4eaba6abfa5ac
mushfiqur rahman <himel6087@gmail.com>
2026-04-28 06:00:53 +0600

    Step 6: Add framework/extractors

---
+++ framework/extractors/__init__.py
@@ -0,0 +1,6 @@
+"""Spec ingestion: parser and chunker that turn markdown into structured input."""
+
+from autospectest.framework.extractors.parser import ParserAgent
+from autospectest.framework.extractors.chunker import ChunkerAgent
+
+__all__ = ["ParserAgent", "ChunkerAgent"]
